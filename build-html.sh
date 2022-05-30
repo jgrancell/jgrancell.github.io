520cda67a6ca7767dcd8655f455db1efd101bb70
Josh Grancell <josh@joshgrancell.com>
2022-05-30 00:14:33 -0500

    Updated regen

--- build-html.sh
+++ build-html.sh
@@ -15,12 +15,15 @@ for FILE_PATH in src/content/**; do
       cat "src/content/${FILE_NAME}"
       cat "src/templates/footer.html"
     } >> "docs/${FILE_NAME}"
-
-    sed -i "s/YEAR_REPLACE/${YEAR_REPLACE}/g" "docs/${FILE_NAME}"
-    sed -i "s/VERS_REPLACE/${VERSION}/g"   "docs/${FILE_NAME}"
-    sed -i "s/DATE_REPLACE/${DATE_REPLACE}/g" "docs/${FILE_NAME}"
-    sed -i "s#CDN_REPLACE#${CDN_REPLACE}#g" "docs/${FILE_NAME}"
-
-    echo "Generated file ${FILE_NAME}"
   fi
+done
+
+for FILE in `find docs/ -type f`; do
+
+    sed -i "s/YEAR_REPLACE/${YEAR_REPLACE}/g" "${FILE}"
+    sed -i "s/VERS_REPLACE/${VERSION}/g"   "${FILE}"
+    sed -i "s/DATE_REPLACE/${DATE_REPLACE}/g" "${FILE}"
+    sed -i "s#CDN_REPLACE#${CDN_REPLACE}#g" "${FILE}"
+
+    echo "Completed renegeration of file ${FILE_NAME}"
 done
